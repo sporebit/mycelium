72c7a00d0518cbe800148fa9964ee2e6f8600af3
Phil <pwhelanonline@gmail.com>
2026-06-21 03:14:10 +0100

    chore: set PC agent metrics secret

--- pc-agent/config.js
+++ pc-agent/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   MYCELIUM_URL: "https://mycelium.sporebit.com/api/studio/pc-metrics",
-  PC_METRICS_SECRET: "REPLACE_WITH_YOUR_SECRET",
+  PC_METRICS_SECRET: "%DtawMqvUw#f^8B3Q%^%sD8j7kjR^P",
   POLL_INTERVAL_MS: 30000,
 };
